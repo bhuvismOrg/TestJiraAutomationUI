506fdafb56a1d10d2bf61e6578a78f9a3b6f0a4f
bhuvism <bhuvanbhuvism@gmail.com>
2022-06-14 22:59:14 +0530

    As on 14/6 Tue

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 //Create node server and listen on port 3000
 var express = require('express');
-var fileupload = require('express-fileupload');
 var crypto = require('crypto')
-var splitFile = require('split-file')
+var cors = require('cors')
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
+});
+
 var app = express();
+//app.use(cors())
+app.use(express.json());
+
 //path to directory where html files are stored
 app.use(express.static('views'));
-app.use(fileupload());
+
 app.get('/', function(req, res){
   res.sendFile('index.html');
 });
@@ -23,45 +34,51 @@ keyDES = crypto.createHash('sha256').update(String(keyDES)).digest('base64').sub
 // encrypt function - AES
 const encryptAES = (buffer) => {
   let iv = crypto.randomBytes(16)
+  
 
   const cipher = crypto.createCipheriv(algorithmAES,keyAES,iv);
+  
   const result = Buffer.concat([iv, cipher.update(buffer), cipher.final()])
-
+  console.log(result);
   return result;
 }
 
 // encrypt function - DES
 const encryptDES = (buffer) => {
   let iv = crypto.randomBytes(8)
+  
 
   const cipher = crypto.createCipheriv(algorithmDES,keyDES,iv);
-  const result = Buffer.concat([iv, cipher.update(buffer), cipher.final()])
+  const result = Buffer.concat([iv, cipher.update(buffer),cipher.final()])
 
   return result;
 }
 
 //decrypt function - AES
-const decryptAES = (encrypted) => {
+const decryptAES = (encrypted,key) => {
   // get iv : the first 16 bytes
   let iv = encrypted.slice(0,16)
+  console.log(iv);
   // get the rest
   encrypted = encrypted.slice(16)
+  console.log(encrypted);
 
-  const decipher = crypto.createDecipheriv(algorithmAES,keyAES,iv);
+  const decipher = crypto.createDecipheriv(algorithmAES,key,iv);
 
-  const result = Buffer.concat([decipher.update(encrypted),decipher.final()])
+  const result = Buffer.concat([decipher.update(encrypted), decipher.final()])
+  
   return result;
 }
 
 
 //decrypt function - DES
-const decryptDES = (encrypted) => {
+const decryptDES = (encrypted,key) => {
   // get iv : the first 16 bytes
   let iv = encrypted.slice(0,8)
   // get the rest
   encrypted = encrypted.slice(8)
 
-  const decipher = crypto.createDecipheriv(algorithmDES,keyDES,iv);
+  const decipher = crypto.createDecipheriv(algorithmDES,key,iv);
 
   const result = Buffer.concat([decipher.update(encrypted),decipher.final()])
   return result;
@@ -69,46 +86,134 @@ const decryptDES = (encrypted) => {
 
 //handle post file upload
 app.post('/upload', function(req, res){
+  var name = req.body.user.name;
+  var base64 = req.body.user.base64
+  var beforeBase64 = req.body.user.beforeBase64
+  console.log(name,base64,beforeBase64);
+
+  console.log("Length => "+ base64.length);
+
+  var data1 = base64.slice(0,base64.length/2);
+  var data2 = base64.slice(base64.length/2,base64.length);
+
+  console.log("Len Data1 => "+data1);
+  console.log("Len Data2 => "+data2);
   
-  let data = req.files.mypic.data.toString()
-  console.log(req.files.mypic.data)
-  let size = req.files.mypic.size
+  let encryptedAES = encryptAES(data1).toString('hex');
+  console.log("encryptedAES => " +encryptedAES.length);
 
-  let data1 = data.slice(0,size/2)
-  let data2 = data.slice(size/2,size)
-  // let data3 = data.slice(2*size/3,size)
+ let encryptedDES = encryptDES(data2).toString('hex');
+ console.log("encryptedDES => " +encryptedDES.length);
 
-  console.log(data1)
-  console.log("divided")
-  console.log(data2)
-  console.log("divided")
-  //console.log(data3)
+  con.connect(function(err){
+    if(err) throw err;
+    var sql = `INSERT INTO info(name,beforeBase64,en1,en2,keyAES,keyDES) VALUES('${name}','${beforeBase64}','${encryptedAES}','${encryptedDES}','${keyAES}','${keyDES}')`;
+    con.query(sql, function(err, result){
+      if(err) throw err;
+      console.log("Inserted");
+  })
+  })
 
-  let encryptedAES = encryptAES(data1);
-  console.log("Encryption of data1:(AES) "+encryptedAES.toString());
-
-  let decryptedAES = decryptAES(encryptedAES)
-  console.log("decryption of data1: "+decryptedAES.toString())
-
-  let encryptedDES = encryptDES(data2);
-  console.log("Encryption of data2:(DES) "+encryptedDES.toString());
-
-  let decryptedDES = decryptDES(encryptedDES)
-  console.log("decryption of data2: "+decryptedDES.toString())
-
-  let resultEn = encryptedAES.toString() + encryptedDES.toString();
-  console.log("Result in encrypted form after combining: "+resultEn)
-  let resultDe = decryptedAES.toString() + decryptedDES.toString();
-  console.log("Result in decrypted form after combining: "+resultDe)
-  // console.log("ss")
-  // let res_data1 = result.slice(0,size/2)
-  // console.log(res_data1)
-  // console.log("ss")
-  // let res_data2 = result.slice(size/2,size)
-  // console.log(res_data2)
-
-  // console.log((res_data1+res_data2))
-  
-  res.send('uploaded');
+  res.send("Uploaded");
 });
+
+app.get('/decrypt', function(req,res){
+  var base64Url = '';
+  var filename;
+  var responseJSON;
+  con.connect( function(err){
+    if(err) throw err;
+    var sql = 'SELECT * FROM info where id=18';
+    con.query(sql, function(err, result){
+      if(err) throw err;
+      console.log(result);
+      let name = result[0].name;
+      let beforeBase64 = result[0].beforeBase64;
+      let en1 = result[0].en1;
+      let en2 = result[0].en2;
+      let key1 = result[0].keyAES;
+      let key2 = result[0].keyDES;
+
+      console.log(name,beforeBase64,en1 ,en2);
+
+    console.log(en1.length, en2.length);
+      let en1Buffer = Buffer.from(en1,'hex')
+      console.log(en1Buffer.length);
+      let decryptedAES = decryptAES(en1Buffer,key1);
+      console.log("decryptedAES => "+decryptedAES); 
+
+      let en2Buffer = Buffer.from(en2,'hex')
+      console.log(en2Buffer.length);
+      let decryptedDES = decryptDES(en2Buffer,key2);
+      console.log("decryptedDES => "+decryptedDES)
+
+      base64Url = beforeBase64 + ',' + decryptedAES + decryptedDES;
+      filename = name;
+      
+      
+      responseJSON = {
+        link:base64Url,
+        name:filename
+      }
+      responseJSON = JSON.stringify(responseJSON)
+      console.log(responseJSON);
+      res.send(responseJSON)
+      
+  })
+  
+  })
+  
+ 
+})
+
+app.post('/register', function(req,res){
+  var name = req.body.user.name;
+  var email = req.body.user.email;
+  var password = req.body.user.pass2;
+  var number = req.body.user.number;
+
+  console.log(name,number);
+
+  con.connect( function(err){
+    if(err) throw err;
+    var sql = `INSERT INTO users(name,email,password,number) VALUES('${name}','${email}','${password}','${number}')`;
+    con.query(sql, function(err, result){
+      if(err) throw err;
+      let responseJSON = {
+        response:"Inserted",
+        
+      }
+      res.send(responseJSON)
+      
+  })
+  
+  })
+})
+
+app.post('/login',function(req,res){
+  var email = req.body.user.email;
+  var password = req.body.user.password;
+
+  console.log(email,password);
+
+  var sql = `SELECT * FROM users WHERE email='${email}' AND password='${password}'`;
+  con.query(sql, function(err,result){
+    if(err) throw err;
+    console.log(result.length);
+    if(result.length > 0){
+      let responseJSON = {
+        response:"Verified",
+        
+      }
+      res.send(responseJSON)
+
+    } else {
+      responseJSON = {
+        response:"Not Verified",
+        
+      }
+      res.send(responseJSON);
+    }
+  })
+})
 app.listen(3000);
