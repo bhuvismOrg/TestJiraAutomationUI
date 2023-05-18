537b7ec9fb077111d41a935e02192ad81f807905
Bhuvan Jain S M <bhuvanbhuvism@gmail.com>
2023-05-19 00:08:36 +0530

    Update test.js

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const app = express();
 
-app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: false}));
 app.use(bodyParser.json());      
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/views'));
