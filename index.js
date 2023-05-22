ad69882c4e32fe91f624f0bd1148d1da4437c6a6
Bhuvan Jain S M <bhuvanbhuvism@gmail.com>
2023-05-22 11:07:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var fs = require('fs')
 var app = express()
 var port = 3000
+var xyz = 5000
 
 fs.appendFile("./uploads/b.txt", "bande", (err,data) => {
     if(err) console.log(err)
